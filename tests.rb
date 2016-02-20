ea3f32b0feb66680226fa23bf36e0426c645679a
Blake Strickland <Stricklandb02@gmail.com>
2016-02-20 09:05:55 -0500

    Lessons are associated with readings, if lesson is destroyed so are readings

--- tests.rb
+++ tests.rb
@@ -1,10 +1,14 @@
 # Basic test requires
 require 'minitest/autorun'
 require 'minitest/pride'
-
+require 'active_record'
+require './school.rb'
+require './term.rb'
 # Include both the migration and the app itself
 require './migration'
 require './application'
+ActiveRecord::Migration.verbose = false
+
 
 # Overwrite the development database connection with a test connection.
 ActiveRecord::Base.establish_connection(
@@ -24,5 +28,42 @@ class ApplicationTest < Minitest::Test
   def test_truth
     assert true
   end
+
+  def test_schools_and_term_relation
+    school = School.create(name: "Education")
+    fall = Term.create()
+    spring = Term.create()
+    school.terms << fall
+    school.terms << spring
+
+    assert_equal [fall, spring], school.terms.all
+  end
+
+  def test_lessons_have_readings
+    lesson = Lesson.create(name: "Integrate databases with Ruby!")
+    reading1 = Reading.create(caption: "How many dots can I get?")
+    reading2 = Reading.create(caption: "I've got a lovely bunch of cocodots, dootaledee")
+    lesson.readings << reading1
+    lesson.readings << reading2
+
+    assert_equal [reading1, reading2], lesson.readings.all
+    lesson.destroy
+
+    assert lesson.destroyed?
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
